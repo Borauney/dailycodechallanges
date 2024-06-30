4fbd98cb806159292a3a59d0f87da02fb92e5fe3
borau <borauney@gmail.com>
2024-06-30 11:36:38 +0300

    questionthree solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static java.lang.reflect.Array.set;
@@ -12,31 +13,28 @@ public class Main {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        list.add(0);
+        
 
-        ArrayList<Integer> result = questiontwo(list);
+        int result = questionthree(list);
         System.out.println(result);
         
     }
 
-    public static ArrayList<Integer> questiontwo(List<Integer>list){
-        ArrayList<Integer> result = new ArrayList<>();
-        result.addAll(list);
+    public static int questionthree(List<Integer>list) {
+        Collections.sort(list);
+        int needed =0;
+        for (int i =0; i<list.size();i++){
 
-        for(int i=0; i<list.size();i++){
-            int product=1;
-            for(int j=0; j<list.size();j++){
-                    if(i!=j){
-                        product*=list.get(j);
-
-                }
+            if(list.get(i)>0 && !list.contains(needed)){
+               return needed = list.get(i)+1;
             }
-            result.set(i,product);
         }
-        return  result;
+
+        return  list.getLast()+1;
+
 
     }
+
 
 }
